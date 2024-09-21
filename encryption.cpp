b38bc2e5646d92b4cfeed342689f05574e9df5a2
AnnaArakelyan1 <annaarakelyan.tt219-2@polytechnic.am>
2024-09-21 18:43:04 +0400

    Create encryption.cpp

---
+++ encryption.cpp
@@ -0,0 +1,71 @@
+//encryption
+#include <iostream>
+#include <string>
+using namespace std;
+
+void print(const string& str, int size) {
+    for (int i = 0; i < size; i++) {
+        cout << str[i];
+    }
+    cout << endl;
+}
+
+string encryption(const string& text, const string& esh, const string& desh, int size) {
+    string encr;
+    for (int i = 0; i < size; i++) {
+        char current = text[i];
+        for (int j = 0; j < 26; j++) {
+            if (current == esh[j]) {
+                encr += desh[j];
+                break;
+            }
+        }
+    }
+    return encr;
+}
+
+
+string decryption(const string& text, const string& esh, const string& desh, int size) {
+    string decr;
+    for (int i = 0; i < size; i++) {
+        char current = text[i];
+        for (int j = 0; j < 26; j++) {
+            if (current == desh[j]) {
+                decr += esh[j];
+                break;
+            }
+        }
+    }
+    return decr;
+}
+
+
+
+int main() {
+     string esh, desh;
+     
+     esh.resize(26); 
+    for (int i = 0; i < 26; i++) {
+        esh[i] = 'a' + i;
+    }
+    print(esh, 26);
+    
+    desh.resize(26); 
+    for (int i = 0; i < 24; i++) {
+        desh[i] = 'c' + i;
+       
+    }
+     desh[24] = 'a';
+      desh[25] = 'b';
+    print(desh, 26);
+    string text;
+    getline(cin,text);
+    cout<<endl<<"Current text:";
+    print(text,text.length());
+    cout<<"encrypted text:";
+    string en=encryption(text,esh,desh,text.length());
+    cout<<en;
+      
+      
+    return 0;
+}
